b535502c4753a52b389fab8ce17ea790a7d3ae11
deshinta <deshintaaulia065@gmail.com>
2026-06-30 20:51:58 +0700

    feat: add get products controller

--- controller/product.js
+++ controller/product.js
@@ -1,6 +1,12 @@
+const {Products} = require('../models')
+
 class ControllerProduct {
   static getProducts(req, res, next) {
     try {
+      let option = {where:{}}
+      const {page,sort,search,filter} = req.query
+      const products = await Products.findAndCountAll()
+      res.status(200).json(products)
     } catch (error) {
       next(error);
     }
